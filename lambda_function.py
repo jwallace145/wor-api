f594eb615cd3365bc37bd8c50868cba431684365
James  Wallace <jameswallace@Jamess-MBP.lan1>
2022-02-17 14:02:59 -0500

    Log event and context

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,15 @@
 import json
 from src.aws_gateways.dynamodb_gateway import DynamoDbGateway
 
+from src.utils.logger import Logger
+
+log = Logger(__name__).get_logger()
+
 
 def lambda_handler(event, context):
+
+    log.info(f"lambda event:\n{json.dumps(event, indent=4)}")
+    log.info(f"lambda context:\n{json.dumps(context, indent=4)}")
 
     dynamodb_gateway = DynamoDbGateway()
 
