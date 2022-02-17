bd18deabc230394e8c534af78c306a3cb1da83ad
James  Wallace <jameswallace@Jamess-MBP.lan1>
2022-02-17 14:57:10 -0500

    Test

--- src/handlers/strike_prices_handler.py
+++ src/handlers/strike_prices_handler.py
@@ -15,5 +15,4 @@ class StrikePriceHandler:
 
     def get_strike_prices(self, email: str) -> List[StrikePrice]:
         strike_prices = self.dynamodb_gateway.get_strike_prices(email=email)
-        strike_prices = [vars(strike_price) for strike_price in strike_prices]
         return [vars(strike_price) for strike_price in strike_prices]
